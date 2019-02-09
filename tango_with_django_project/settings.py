2a837f1805e4fc4157608b2ef064b06576d838b4
Caylin Faye McCalpin <rosieandpip@gmail.com>
2019-02-09 21:38:31 +0000

    solved collision, added index

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'tango_with_django_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATES_DIR,],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
