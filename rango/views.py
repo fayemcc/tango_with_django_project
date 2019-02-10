6f06427e89738045bb0fcbca536d6f958fd8bc08
Caylin Faye McCalpin <rosieandpip@gmail.com>
2019-02-10 18:11:30 +0000

    changed templates directory

--- rango/views.py
+++ rango/views.py
@@ -1,7 +1,12 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+
 
 def index(request):
-    return HttpResponse("Rango says hey there partner! <a href='http://127.0.0.1:8000/rango/about/'>(About Page)<a href>")
+    context_dict = {'boldmessage': "Crunch, creamy, cookie, candy, cupcake!"}
+
+    return render(request, 'rango/index.html', context=context_dict)
+
 
 def about(request):
     return HttpResponse("Rango says here is the about page. <a href='http://127.0.0.1:8000/rango/'>(Home Page) <a href>")
