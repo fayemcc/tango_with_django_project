e9aba719689b7815ac7998c1ad3a160a408c654e
Caylin Faye McCalpin <rosieandpip@gmail.com>
2019-02-11 18:08:07 +0000

    copied in through admin update

--- populate_rango.py
+++ populate_rango.py
@@ -31,20 +31,25 @@ def populate():
         {"title": "Flask", "url": "http://flask.pocoo.org"}
     ]
 
-    cats = {"Python": {"pages": python_pages}, "Django": {"pages": django_pages}, "Other Frameworks": {"pages": other_pages}}
+    cats = {"Python": {"pages": python_pages, "views": 128, "likes": 64}, "Django": {"pages": django_pages, "views": 64, "likes": 32}, "Other Frameworks": {"pages": other_pages, "views": 32, "likes": 16}}
 
     for cat, cat_dict in cats.items():
-        c = add_cat(cat)
+        c = add_cat(cat, cat_dict["views"], cat_dict["likes"])
         for p in cat_dict["pages"]:
             add_page(c, p["title"], p["url"])
 
     for c in Category.objects.all():
+        print("<Category: {0}, {1} views, {2} likes".format(str(c), c.views, c.likes))
         for p in Page.objects.filter(category=c):
             print("- {0}- {1}".format(str(c), str(p)))
 
 
-def add_cat(name):
+def add_cat(name, views, likes):
     c = Category.objects.get_or_create(name=name)[0]
+    if not views == 0:
+        c.views = views
+    if not likes == 0:
+        c.likes = likes
     c.save()
     return c
 
--- rango/views.py
+++ rango/views.py
@@ -1,9 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from rango.models import Category, Page
+
 
 def index(request):
-    context_dict = {'boldmessage': "Crunch, creamy, cookie, candy, cupcake!"}
+    categories_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {'categories': categories_list}
 
     return render(request, 'rango/index.html', context=context_dict)
 
